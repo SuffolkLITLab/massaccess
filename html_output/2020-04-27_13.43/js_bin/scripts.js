20fdba82af001dd46a342f86da5c801cb2a4baad
David Colarusso <davidcolarusso@Liz-3.local>
2020-04-27 15:41:08 -0400

    2020-04-27_15.35 update

--- html_output/2020-04-27_13.43/js_bin/scripts.js
+++ html_output/2020-04-27_13.43/js_bin/scripts.js
@@ -1,5 +1,6 @@
 function test_test(text) {
-  if (window.location.href.indexOf("/massaccess/html_output/") > -1) {
+  //if (window.location.href.indexOf("/massaccess/html_output/") > -1) {
+  if(1==1) {
     $('#alert_test').html(text);
     $('#alert_test').show();
     return true;
